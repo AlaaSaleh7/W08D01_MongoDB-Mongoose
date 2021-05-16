d5d803b29bd538a6ed8aaf023dd50f528346511d
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-16 12:47:32 +0300

    pulse check Q2

--- db.js
+++ db.js
@@ -1,1 +1,18 @@
 const mongoose = require ('mongoose');
+
+const options ={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+};
+
+//connecting mongoose
+mongoose.connect('mongodb://localhost:27017/DB_todos',options).then(
+    ()=>{
+        console.log('DB ready to use')
+    },
+    (err)=>{
+        console.log(err);
+    }
+
+);
