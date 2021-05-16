832ab4b05ddb544e4918c3223673d872be72080c
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-16 13:09:41 +0300

    pulse check Q3

--- schema.js
+++ schema.js
@@ -1,1 +1,9 @@
 const mongoose = require ('mongoose');
+
+const todoSchema = new mongoose.Schema({
+    task:{type:String,require:true,unique:true},
+    description:{type:String},
+    deadline:{type:Date,require:true},
+    isCompleted:{type:String,require:true},
+    priority:{type:String,require:true},
+});
