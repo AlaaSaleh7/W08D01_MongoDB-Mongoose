31fb3c9af7d1c9d05e815a9e1fb3657288d8b32f
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-16 12:39:18 +0300

    pulse check Q1

---
+++ app.js
@@ -0,0 +1,33 @@
+// pulse check
+// Q1
+const express = require('express');
+
+const todoModule = require('./schema');
+
+const db = require('./db');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/todos',(req,res)=>{
+
+});
+
+app.post('/create/todo',(req,res)=>{
+
+});
+
+app.put('/update/todo',(req,res)=>{
+
+});
+
+app.delete('/delete/todo',(req,res)=>{
+
+});
+
+const port = 3000;
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
